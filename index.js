78b7edaf4b3092596f0bcb7047b32e7b1da3709e
avmaxim <avmax19@gmail.com>
2021-10-20 11:37:04 +0300

    Revert back secretsInTokenUri param for Apple

--- index.js
+++ index.js
@@ -54,6 +54,7 @@ const oauthPlugin = fp(function (fastify, options, next) {
 
   const name = options.name
   const credentials = options.credentials
+  const secretsInTokenUri = options.secretsInTokenUri || false
   const callbackUri = options.callbackUri
   const callbackUriParams = options.callbackUriParams || {}
   const scope = options.scope
@@ -91,7 +92,12 @@ const oauthPlugin = fp(function (fastify, options, next) {
     )
     const getTokenParams = {
       code: code,
-      redirect_uri: callbackUri
+      redirect_uri: callbackUri,
+      ...(secretsInTokenUri && {
+        client_id: credentials.client.id,
+        client_secret: credentials.client.secret,
+        grant_type: 'authorization_code'
+      })
     }
     console.log(
       '[FASTIFY OAUTH2 LIBRARY]: ',
